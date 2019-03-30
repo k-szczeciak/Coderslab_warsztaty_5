aabd8b323ff849dd161698f4f61cc8df15e42bbb
k-szczeciak <szczeciak@hotmail.com>
2019-03-30 15:45:31 +0100

    zadanie 4

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -7,4 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/books")
 public class BookController { @RequestMapping("/hello") public String hello(){
 return "{hello: World}"; }
+
+
+    @RequestMapping("/helloBook") public Book helloBook(){
+        return new Book(1L,"9788324631766","Thinking in Java", "Bruce Eckel","Helion","programming");
+    }
+
 }
